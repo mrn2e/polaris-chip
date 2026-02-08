c9c41cb08c1710f2817e6d28db2faa8fc69705fe
Mayita <oglamegamars@gmail.com>
2026-02-07 21:45:02 -0500

    the whole thing
    I'm gonna get better and committing early and often, I swear.

--- src/my-card.js
+++ src/my-card.js
@@ -12,26 +12,100 @@ export class MyCard extends LitElement {
     return 'my-card';
   }
 
-  constructor() {
-    super();
-    this.title = "My card";
-  }
-
   static get styles() {
     return css`
       :host {
-        display: block;
+        .card {
+  background-color: var(--my-card-bg-color, #A44A3F);
+  max-width: 400px;
+  margin: 0 auto;
+  display: inline-block;
+  padding: 0;
+}
+
+.header {
+  font-family: 'Roboto', sans-serif;
+  font-size: 34px;
+  font-weight: bold;
+  margin: 20px 100px;
+  color: #F19C79;
+}
+
+.desc {
+  font-family: 'Roboto', sans-serif;
+  font-size: 18px;
+  font-weight: bold;
+  margin: 20px;
+  color: #F19C79;
+  
+}
+
+.btnn {
+  display: none;
+  background-color: #F6F4D2;
+  color: #CBDFBD;
+  font-size: 30px;
+  margin: 10px 120px;
+  padding: 20px;
+  border: 10px solid #F54927;
+}
+
+.item {
+  max-width: 300px;
+  margin: 50px;
+  margin-bottom: 7px;
+}
+
+@media screen and (min-width: 500px) and (max-width: 800px) {
+  button{
+    display: block;
+  }
+}
+
+@media screen and (max-width: 500px) {
+  .card {
+    transform: scale(0.8);
+    transform-origin: top center;
+  }
+}
+
       }
     `;
   }
 
+  constructor() {
+    super();
+    this.title = "My card";
+    this.img = "https://oer.hax.psu.edu/mjr7285/sites/hwblog/files/Screenshot%202026-01-22%201420111.png";
+    this.desc = "IMG Description: Meme created in part one of homework. Further notes found on HW pt.1 blog post"
+    this.alt = "Meme made in HW week one, part one";
+
+  }
+
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`<div class="card">
+  <div class="wrapper">
+  <h1 class="header">Simple Card</h1>
+<div class="card__btn">   
+  <a href="https://hax.psu.edu" class="details-btn">
+    <button class="btnn"> Details </button>
+  </a>
+</div>
+<div class="card__img">
+  <img class="item" src="${this.img}" alt="${this.alt}"/>
+  <p class="desc">${this.desc}</p>
+</div>  
+  </div>
+</div>
+`;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      img: { type: String },
+      desc: { type: String },
+      alt: { type: String },
     };
   }
 }
--- src/polaris-chip.js
+++ src/polaris-chip.js
@@ -29,6 +29,7 @@ export class PolarisChip extends LitElement {
     super();
     // a variable on this object called title
     this.title = 'Chip Default';
+    this.link = 'https://example.com'; //just to check if working
   }
 
   // CSS styles are scoped JUST to this element. This uses a technology called
@@ -50,14 +51,15 @@ export class PolarisChip extends LitElement {
       }
 
       span {
-        background-color: orange;
+        background-color: yellow;
         color: black;
         font-size: 24px;
         padding: 16px;
         margin: 8px;
       }
-
-      span:hover {
+      
+      a:focus-within,
+      a:hover {
         background-color: grey;
         border: 1px solid black;
       }
@@ -83,7 +85,8 @@ export class PolarisChip extends LitElement {
     // it is going to print the title of the element. The magic of Lit is that
     // when title is changed (even by inspecting the document and hacking the value)
     // it will automatically update what is displayed and do so incredibly quickly
-    return html`<span>${this.title}</span>`;
+    
+    return html`<span></span><a href="${this.link}">${this.title}</a></span>`; 
   }
 
   // LitElement uses the properties call to do the following:
@@ -95,6 +98,7 @@ export class PolarisChip extends LitElement {
     return {
       // this is a String. Array, Object, Number, Boolean are other valid values here
       title: { type: String },
+      link: { type: String },
     };
   }
 }
